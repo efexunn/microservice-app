4a099c38e9e1bf86cf3f13c57f0bd81dd18d9b7a
Fevzihan Yusuf Bozkurt <105756054+efexunn@users.noreply.github.com>
2025-03-20 17:20:02 +0300

    auth service and configs implemented

---
+++ services/auth/src/test/java/com/efexunn/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efexunn.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
